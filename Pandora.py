07f2e2bcf13c91ca3c61111313661c1fb1ffc387
vincebye <704336673@qq.com>
2016-09-21 00:57:18 +0800

    基本完成单线程网页抓取功能,PANDORAv1.0beta版本

--- Pandora.py
+++ Pandora.py
@@ -6,14 +6,14 @@ import os
 import Queue
 from bs4 import BeautifulSoup
 import datetime
-
+import urlparse
+import urlparse
 queone=Queue.Queue(maxsize=0)   #unlimited length
 quetwo=Queue.Queue(maxsize=0)   #echecked
 
-req_header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 'Accept':'text/html;q=0.9,*/*;q=0.8', 
+req_header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 
+'Accept':'text/html;q=0.9,*/*;q=0.8', 
 'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding':'gzip', 'Connection':'close', 'Referer':None } 
-
-
 
 def spider_web(url,queueue):
 	try:
@@ -36,16 +36,18 @@ def spider_web(url,queueue):
     
 	for url_link in url_data_link:
 		queue_temp=url_link.get('href')
-		if check_source_url(queue_temp):
-		    queueue.put(queue_temp) 
+		if check_tian_url(queue_temp):
+			if check_source_url(queue_temp):
+				queueue.put(queue_temp) 
 
 	
     # 把a标签中的href属性值都放进队列一
         
 	for url_a in url_data_a:
 		queue_temp=url_a.get('href')
-		if check_source_url(queue_temp):
-		    queueue.put(queue_temp)
+		if check_tian_url(queue_temp):
+			if check_source_url(queue_temp):
+				queueue.put(queue_temp)
 	check_repeat_url(queueue)
 
 	# 经检查之后的url放进队列二
@@ -60,6 +62,12 @@ def swap_queue(queone,quetwo):
 		quetwo.put(queone.get())
 
 	pass
+def check_tian_url(url):
+	m=re.match('.*?(\#)',str(url))
+	if not m:
+		return 1
+	else:
+		return 0
 
 def analyse_web():
 	for url_num in range(0,queone.qsize()):
@@ -68,23 +76,50 @@ def analyse_web():
 
 # 检查url是否属于同源域名        
 def check_source_url(url):
-	if(url!=None):
-		url_temp=url.split('.',2)
-		print url_temp[1]
-		if(url_temp[1]==url_aim[1]):
+	if(url!=None and url!=' '):
+		url_source_temp=url.split('.',2)
+		#print url_source_temp
+		if(url_source_temp[1]==url_aim[1]):
 			return 1
 		else:
 			return 0
 
-		#m=re.match(r'www.(.*?), url)
-
+		
+#检测重复url
 def check_repeat_url(quesource):
 	url_list=[]
+	url_param_list=[]
 	time=0
 	while(quesource.qsize()):
 		queue_temp=quesource.get()
-		if queue_temp not in url_list:
-		    url_list.append(queue_temp)
+		m=check_param_url(queue_temp)
+
+		#检测网页是否有参数，若有参数获取参数
+		if check_param_url(queue_temp):
+			url_param_temp=get_param_url(queue_temp)
+			if url_param_temp not in url_param_list:
+				url_param_list.append(url_param_temp)
+				#检测是否url是否重复，若重复则不加入到列表中	
+				if queue_temp not in url_list: 
+					url_list.append(queue_temp)
+					print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+					print url_list
+					print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+
+
+		elif queue_temp not in url_list:
+			# print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+			# print m
+			# print "elif"
+			# print queue_temp
+			# print url_list
+			# print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+		  	url_list.append(queue_temp)
+
+
+		
+	#把列表中的元素重新放回队列    
 	while len(url_list):
 		for i in range(0,len(url_list)):
 			quesource.put(url_list[i])
@@ -92,7 +127,22 @@ def check_repeat_url(quesource):
 			if time==len(url_list):
 				return 
 			
-
+#检测参数url
+def check_param_url(url):
+	if urlparse.urlparse(str(url)).query=='':
+		return 0
+	else:
+		return 1
+#获取url参数
+def get_param_url(url):
+	param_url_temp=urlparse.urlparse(str(url)).query
+	m=re.findall('[?|&]*(.*?)=',str(param_url_temp))
+	try:
+		#print m[0]
+		return m[0]
+	except:
+		return 0
+	
 
 
 
@@ -109,6 +159,7 @@ def main():
     spider_web('http://'+urladdress+'/',queone)
     run_time=int(raw_input(r'次元の沙漏>>'))
     while(run_time-1):
+    	print ">>>>>>>"+str(run_time)
         while(queone.qsize()):
         	try:
         		spider_web(queone.get(), quetwo)
@@ -119,6 +170,7 @@ def main():
         		pass
         swap_queue(quetwo, queone)
         run_time=run_time-1
+    check_repeat_url(queone)
 
     # while(queone.qsize()):
     # 	print queone.get()
