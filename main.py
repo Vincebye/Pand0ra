4b032c28cd366a67e211aa7dce0e90b2bda7c892
v1ce0ye <704336673@qq.com>
2020-03-22 23:52:22 +0800

    爬取SPA

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*-
-import sys 
-sys.path.append("lib")
-from lib.urlfilter import*
-from lib.urlcrawl import*
-import os
-import os.path
-import datetime 
-reload(sys)
-sys.setdefaultencoding('utf8')  
+import configparser
+cfg = configparser.ConfigParser()
+cfg.read('pand0ra.conf')
 
-if __name__ == '__main__':
-	starttime=datetime.datetime.now()
-	url=raw_input(r'次元の圣光（ex:http://www.exaple.com）>>')
-	run_time=int(raw_input(r'次元の沙漏>>'))
-	crawl=urlcrawl(url, run_time)
-	crawl.crawl()
-	endtime=datetime.datetime.now()
-	print '时间の轨迹>>'+str((endtime-starttime).seconds)+'s'
-	
+crawled=[]#记录爬取过的URL
+class Do:
+    def __init__(self):
+        print('1')
+    def speak(self,dd):
+        print(dd)
+    def __del__(self):
+        print('2')
+
+a=Do()
